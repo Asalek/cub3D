c95a9d164b9807e666587bf26e82535a4aa9c852
asalek <ayoub.salek8599@gmail.com>
2022-08-19 21:33:20 +0100

    fix: movement to wall

--- mandatory/rayCasting/movement_rotate.c
+++ mandatory/rayCasting/movement_rotate.c
@@ -6,7 +6,7 @@
 /*   By: asalek <asalek@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/05 19:23:44 by asalek            #+#    #+#             */
-/*   Updated: 2022/08/09 22:57:34 by asalek           ###   ########.fr       */
+/*   Updated: 2022/08/19 21:32:34 by asalek           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,12 +80,15 @@ void	s_key(t_all *t)
 {
 	if (t->mlx->analog.s == 1)
 	{
-		if (t->map[(int)(t->ray->posx - SPEED * t->ray->dirx)] \
-		[(int)t->ray->posy] != '1')
-			t->ray->posx = t->ray->posx - SPEED * t->ray->dirx;
-		if (t->map[(int)t->ray->posx] \
-		[(int)(t->ray->posy - t->ray->diry * SPEED)] != '1')
-			t->ray->posy = t->ray->posy - SPEED * t->ray->diry;
+		// if (t->map[(int)(t->ray->posx - SPEED * t->ray->dirx)] \
+		// [(int)t->ray->posy] != '1')
+		// 	t->ray->posx = t->ray->posx - SPEED * t->ray->dirx;
+		// if (t->map[(int)t->ray->posx] \
+		// [(int)(t->ray->posy - t->ray->diry * SPEED)] != '1')
+		// 	t->ray->posy = t->ray->posy - SPEED * t->ray->diry;
+		if(t->map[(int)(t->ray->posx - t->ray->dirx * SPEED)]\
+			[(int)(t->ray->posx)] != '1') t->ray->posx -= t->ray->drawend * SPEED;
+		
 	}
 	if (t->mlx->analog.shift == 1 && t->mlx->analog.w == 1)
 	{
